d5689656ab76c31a3535bdcbbc7c6fa56eb23dd2
Charles Nicholson <charles.nicholson@gmail.com>
2024-07-05 10:56:50 -0400

    repro

---
+++ company.bar/company/bar/bar.py
@@ -0,0 +1,5 @@
+from company.foo import foo_func
+
+def bar_func() -> None:
+    foo_func()
+    print("bar_func")
---
+++ repro.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eEuo pipefail
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+python3.11 -m venv "$SCRIPT_DIR/venv"
+"$SCRIPT_DIR/venv/bin/python" -m pip install deptry 
+"$SCRIPT_DIR/venv/bin/python" -m pip install -e "$SCRIPT_DIR/company.foo"
+"$SCRIPT_DIR/venv/bin/python" -m pip install -e "$SCRIPT_DIR/company.bar"
+
+# deptry complains about pyproject.toml not being found when ROOT isn't ".", bug?
+# "$SCRIPT_DIR/venv/bin/python" -m deptry "$SCRIPT_DIR/company.bar" 
+
+echo "Running deptry on company.foo..."
+$(cd "$SCRIPT_DIR/company.foo" && "$SCRIPT_DIR/venv/bin/python" -m deptry .)
+
+echo "Running deptry on company.bar..."
+$(cd "$SCRIPT_DIR/company.bar" && "$SCRIPT_DIR/venv/bin/python" -m deptry .)
