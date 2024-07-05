69906f6fa0de3760d787e40c0535544eb83a75c9
Charles Nicholson <charles.nicholson@gmail.com>
2024-07-05 11:00:46 -0400

    run code to make sure it works

--- repro.sh
+++ repro.sh
@@ -8,6 +8,9 @@ python3.11 -m venv "$SCRIPT_DIR/venv"
 "$SCRIPT_DIR/venv/bin/python" -m pip install -e "$SCRIPT_DIR/company.foo"
 "$SCRIPT_DIR/venv/bin/python" -m pip install -e "$SCRIPT_DIR/company.bar"
 
+echo "Running company.bar.bar_func..."
+"$SCRIPT_DIR/venv/bin/python" -c 'import company.bar;company.bar.bar_func()'
+
 echo "Running deptry on company.bar..."
 # deptry fails with pyproject.toml not being found when ROOT isn't ".", bug?
 #"$SCRIPT_DIR/venv/bin/python" -m deptry "$SCRIPT_DIR/company.bar" 
