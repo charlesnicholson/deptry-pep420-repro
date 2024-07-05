a8f1fcd7486dd8a70711956254f7e59b91340747
Charles Nicholson <charles.nicholson@gmail.com>
2024-07-05 10:58:07 -0400

    simplify

--- repro.sh
+++ repro.sh
@@ -8,11 +8,7 @@ python3.11 -m venv "$SCRIPT_DIR/venv"
 "$SCRIPT_DIR/venv/bin/python" -m pip install -e "$SCRIPT_DIR/company.foo"
 "$SCRIPT_DIR/venv/bin/python" -m pip install -e "$SCRIPT_DIR/company.bar"
 
-# deptry complains about pyproject.toml not being found when ROOT isn't ".", bug?
-# "$SCRIPT_DIR/venv/bin/python" -m deptry "$SCRIPT_DIR/company.bar" 
-
-echo "Running deptry on company.foo..."
-$(cd "$SCRIPT_DIR/company.foo" && "$SCRIPT_DIR/venv/bin/python" -m deptry .)
-
 echo "Running deptry on company.bar..."
+# deptry fails with pyproject.toml not being found when ROOT isn't ".", bug?
+#"$SCRIPT_DIR/venv/bin/python" -m deptry "$SCRIPT_DIR/company.bar" 
 $(cd "$SCRIPT_DIR/company.bar" && "$SCRIPT_DIR/venv/bin/python" -m deptry .)
